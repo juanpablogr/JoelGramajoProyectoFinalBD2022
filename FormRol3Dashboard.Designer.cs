2d860bfab7bb120e338193d329456c4d20780057
Joel Gramajo <78053680+juanpablogr@users.noreply.github.com>
2022-05-24 23:51:42 -0600

    Ultimos cambios

--- FormRol3Dashboard.Designer.cs
+++ FormRol3Dashboard.Designer.cs
@@ -32,6 +32,11 @@
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label4
@@ -71,11 +76,61 @@
             this.label1.TabIndex = 6;
             this.label1.Text = "Bienvenido";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(73, 208);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 91);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Lista de recaudo entre dos fechas";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(282, 208);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(154, 91);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Avance de competencia";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(514, 208);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(154, 91);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Participantes en un torneo";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(73, 363);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(154, 91);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Top 5 participantes";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(282, 363);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(154, 91);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Torneos pendientes de realizar";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // FormRol3Dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 613);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -93,5 +148,10 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label1;
         public System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
